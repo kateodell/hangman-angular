115ec344b67972553953d31966a829a9a94b475d
Kate Odell <kate.m.odell@gmail.com>
2014-04-15 00:10:12 -0700

    Clean up unused controller code

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,7 +6,6 @@ angular.module('hangman.controllers', []).
   controller('GameController', ['$scope', '$http', function($scope, $http) {
 
     $scope.startNewGame = function(){
-      if($scope.gameData == null || $scope.gameData.state != 'alive'){
         console.log("creating game...")
         $http.jsonp('http://hangman.coursera.org/hangman/game?callback=JSON_CALLBACK',
           { params: {
@@ -21,21 +20,10 @@ angular.module('hangman.controllers', []).
           .error(function(data) {
             console.log('Error: ' + data);
         });
-
-        // $scope.gameData = {
-        //   "game_key":"DOHzvCDZVYQE7i07pycC0SFKC6rrbHxZ",
-        //   "phrase":"____________ ___ _____ ________",
-        //   "state":"alive",
-        //   "num_tries_left":"5"
-        // }
-
-      } else {
-        alert('you cant start a new game without finishing this one!')
-      }
-
     };
 
     $scope.makeGuess = function(letter) {
+
       console.log("guessing: " + letter);
       $http.jsonp("http://hangman.coursera.org/hangman/game/" + $scope.gameData.game_key + "?callback=JSON_CALLBACK",
         { params: {
@@ -50,22 +38,9 @@ angular.module('hangman.controllers', []).
         .error(function(data) {
       	console.log('Error: ' + data);
       });
-      // $scope.gameData = {
-      //   "game_key":"DOHzvCDZVYQE7i07pycC0SFKC6rrbHxZ",
-      //   "phrase":"____________ ___ _____ ______e_",
-      //   "state":"alive",
-      //   "num_tries_left":"5"
-      // };
     };
 
     $scope.noCurrentGame = function() {
       return $scope.gameData == null || $scope.gameData.state != 'alive';
-    }
-
-    $scope.checkGameOver = function() {
-      if($scope.gameData.state == "won") {
-
-      }
-    }
-
+    };
   }]);
